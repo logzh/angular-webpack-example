2d307b3f28b89a52616d99653a3ba8b72d111798
logzh <zjhcug@qq.com>
2015-10-29 17:35:51 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { hello: 'Express' });
+  res.render('index', { hello: 'Angularjs webpack example' });
 });
 
 module.exports = router;
