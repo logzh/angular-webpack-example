8314a22b761b821323f4d951926470dd3edaff60
logzh <zjhcug@qq.com>
2016-03-13 16:03:34 +0800

    eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require("gulp");
-var gutil = require("gulp-util");
+var gulp = require('gulp');
+var gutil = require('gulp-util');
 var minifyCss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var eslint = require('gulp-eslint');
@@ -7,26 +7,26 @@ var reporter = require('eslint-html-reporter');
 var fs = require('fs');
 var path = require('path');
 
-var webpack = require("webpack");
-
-var webpackConfigProduct = require("./webpack.production.config.js");
-var webpackConfigDevelop = require("./webpack.development.config.js");
-
-gulp.task("webpack", function(callback) {
-  webpack(webpackConfigProduct, function(err, stats) {
-    if (err) throw new gutil.PluginError("webpack", err);
-    callback();
-  });
-});
-
-gulp.task("webpackDevelop", function(callback) {
-  webpack(webpackConfigDevelop, function(err, stats) {
-    if (err) throw new gutil.PluginError("webpack", err);
-    callback();
-  });
-});
-
 var srcJsDir = './public/static/js/';
+
+var webpack = require('webpack');
+
+var webpackConfigProduct = require('./webpack.production.config.js');
+var webpackConfigDevelop = require('./webpack.development.config.js');
+
+gulp.task('webpack', function(callback) {
+  webpack(webpackConfigProduct, function(err) { // stats
+    if (err) throw new gutil.PluginError('webpack', err);
+    callback();
+  });
+});
+
+gulp.task('webpackDevelop', function(callback) {
+  webpack(webpackConfigDevelop, function(err) { // stats
+    if (err) throw new gutil.PluginError('webpack', err);
+    callback();
+  });
+});
 
 gulp.task('lint', function() {
   return gulp.src([srcJsDir + '**/*.js'])
@@ -37,26 +37,26 @@ gulp.task('lint', function() {
       );
 });
 
-gulp.task("minifyJs", ['webpack'], function() {
-  return gulp.src("./product/**/*.js")
+gulp.task('minifyJs', ['webpack'], function() {
+  return gulp.src('./product/**/*.js')
       .pipe(uglify({
         output: {
           max_line_len: 100
         }
       }))
-      .pipe(gulp.dest("./product"));
+      .pipe(gulp.dest('./product'));
 });
 
-gulp.task("minifycssPro", ['webpack'], function() {
-  return gulp.src("./product/**/*.css")
+gulp.task('minifycssPro', ['webpack'], function() {
+  return gulp.src('./product/**/*.css')
       .pipe(minifyCss())
-      .pipe(gulp.dest("./product"));
+      .pipe(gulp.dest('./product'));
 });
 
-gulp.task("minifycssDev", ['webpackDevelop'], function() {
-  return gulp.src("./development/**/*.css")
+gulp.task('minifycssDev', ['webpackDevelop'], function() {
+  return gulp.src('./development/**/*.css')
       .pipe(minifyCss())
-      .pipe(gulp.dest("./development"));
+      .pipe(gulp.dest('./development'));
 });
 
 gulp.task('copyJson', function() {
